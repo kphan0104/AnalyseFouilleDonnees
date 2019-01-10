b7440373b067bd4329af2f09c9b216923c4204ed
phanke18 <phanke18@b048-24.crio.dauphine.fr>
2019-01-10 09:35:21 +0100

    push

--- tp.py
+++ tp.py
@@ -1,26 +1,24 @@
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 import sklearn
-import itertools as it
 
+# Génération de données
 mu0 = (0,0)
 mu1 = (3,2)
 sigma = [[1,1/2],[1/2,1]]
 
-numpy.random.seed(123)
+np.random.seed(123)
 
-c0App = numpy.random.multivariate_normal(mu0, sigma, 10)
-c1App = numpy.random.multivariate_normal(mu1, sigma, 10)
-app
-c0Test = numpy.random.multivariate_normal(mu0, sigma, 1000)
-c1Test = numpy.random.multivariate_normal(mu1, sigma, 1000)
-test = c0Test + c1Test 
+c0App = np.random.multivariate_normal(mu0, sigma, 10)
+c1App = np.random.multivariate_normal(mu1, sigma, 10)
+app = np.concatenate([c0App,c1App])
 
-
+c0Test = np.random.multivariate_normal(mu0, sigma, 1000)
+c1Test = np.random.multivariate_normal(mu1, sigma, 1000)
+test = np.concatenate([c0Test,c1Test])
 
 c0AppX = [x[0] for x in c0App]
 c0AppY = [x[1] for x in c0App]
-
 
 c0TestX = [x[0] for x in c0Test]
 c0TestY = [x[1] for x in c0Test]
@@ -37,3 +35,7 @@ plt.scatter(c0TestX, c0TestY)
 plt.scatter(c1TestX, c1TestY)
 
 plt.show()
+
+# Analyse Discriminante Linéaire
+
+
